603fcceeb49c8790f4e7d12c82dec10efd9e9eed
Angel Rafael Rodriguez Hernandez <angel01rdz@outlook.com>
2021-09-27 16:18:46 -0500

    Updated Content and added small animations

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,9 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 
+const about = document.querySelector('#about-mobile');
+const contact = document.querySelector('#contact-mobile');
+
 btnHamburger.addEventListener('click', function(){
 
     if (header.classList.contains('open')) {    //close hamburger
@@ -13,4 +16,22 @@ btnHamburger.addEventListener('click', function(){
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
     } 
+});
+
+//This closes the menu when clicking a nav link that leads to the same page
+
+about.addEventListener('click', function(){
+    if (header.classList.contains('open')) {
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+});
+
+contact.addEventListener('click', function(){
+    if (header.classList.contains('open')) {
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
 });
