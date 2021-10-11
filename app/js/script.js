2c0f8764f5b9d472f854db40ac34e72ae822248b
Angel Rafael Rodriguez Hernandez <angel01rdz@outlook.com>
2021-10-11 17:59:44 -0500

    Animation changes

--- app/js/script.js
+++ app/js/script.js
@@ -36,18 +36,26 @@ contact.addEventListener('click', function(){
     }
 });
 
-const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+//////////// GSAP ANIMATIONS ////////////
 
-//intro
+        //// HOME PAGE ////
+
+/////////   Intro section animations
+
+const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
 tl.to('#logo', {opacity: 0, duration: .5, delay: 3})
 tl.from('.intro', {y: '0%', duration: 1.5,}, '-=.5')
 
 
-//Hero -----
+/////////   Hero section animations
+
 tl.from('.main-nav li, .logo, .nav-socials', {opacity: 0, duration: 2.5}, '-=1');
 tl.from('.hide_title', {opacity: 0, duration: 2.5}, '-=3.5');
 tl.from('.hero .btn', {opacity: 0, duration: 1.5}, '-=3.5');
+
+
+/////////   Work section animations
 
 let stTl = gsap.timeline({
     scrollTrigger: {
@@ -60,7 +68,7 @@ if (window.innerWidth < 700) {
    stTl = gsap.timeline({
         scrollTrigger: {
             trigger: '.work',
-            start: 'top top'
+            start: 'top center'
         }
     });
 }
@@ -69,3 +77,72 @@ stTl.from('.work_h2', {y: 300, opacity: 0, duration: 1.5});
 stTl.from('.img-container', {x: '-100%', opacity: 0, duration: 1, stagger: 0.25}, '-=1')
 stTl.from('.work .button', {opacity: 0, duration: 1.5}, '-=1');
 
+////////   About Section Animation 
+
+let stAbout = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.about',
+        start: 'center bottom'
+    }
+});
+
+if (window.innerWidth < 700) {
+    stAbout = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.about',
+            start: 'top center'
+        }
+    });
+}
+
+stAbout.from('.title_text', {opacity: 0, duration: 1});
+stAbout.from('.about img', {opacity: 0, duration: 1}, '-=1');
+stAbout.to('.ab_title_hide', {x: 0, duration: 1.5}, '-=1');
+stAbout.from('.about p', {opacity: 0, duration: 1}, '-=1');
+
+////////   Skills Section Animation 
+
+let stSkills = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.skills',
+        start: 'center bottom'
+    }
+});
+
+if (window.innerWidth < 700) {
+   stSkills = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.skills',
+            start: 'top center'
+        }
+    });
+}
+
+stSkills.from('.skills h1', {y: 300, opacity: 0, duration: 1});
+stSkills.from('.skills__list div', {opacity: 0, duration: 1.5, stagger: .25}, '-=1')
+
+////////   Contact Section Animation
+
+let stContact = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.contact',
+        start: 'center bottom'
+    }
+});
+
+stContact.from('.contact', {opacity: 0, duration: 1});
+
+////////   Topbtn and footer Section Animation
+
+let stFooter = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.to_top',
+        start: 'center bottom'
+    }
+});
+
+stFooter.from('.to_top, footer', {opacity: 0, duration: 1})
+
+        //// WORK PAGE ////
+
+/////////   nav section animations
